222dee360a16b51d5e5a1b20c4a5b013470370fa
Siddu-009 <siddardha9988@gmail.com>
2026-06-13 15:59:11 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -1,8 +1,8 @@
 sudo apt update
+sudo apt install openjdk-21-jdk -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee /etc/apt/keyrings/jenkins-keyring.asc > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt install openjdk-21-jdk -y
 sudo apt install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
