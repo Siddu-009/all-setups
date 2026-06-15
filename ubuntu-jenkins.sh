b0edc5ab6822b03692d9c369149b0839a86a8f49
Siddu-009 <siddardha9988@gmail.com>
2026-06-15 10:53:19 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -1,22 +1,13 @@
-'''sudo apt update
-sudo apt install openjdk-21-jdk -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee /etc/apt/keyrings/jenkins-keyring.asc > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt install jenkins -y
-sudo systemctl daemon-reload
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins'''
+#! /bin/bash
+sudo apt update -y
+sudo apt install -y openjdk-21-jdk
+java -version
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
-sudo apt install openjdk-21-jdk -y
-sudo rm -f /etc/apt/sources.list.d/jenkins.list
-sudo rm -f /etc/apt/keyrings/jenkins-keyring.asc
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /etc/apt/keyrings/jenkins-keyring.asc > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt install jenkins -y
-sudo systemctl daemon-reload
-sudo systemctl enable jenkins
+sudo apt install jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
